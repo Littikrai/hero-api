0900f9c85aabfb723c02a086bd87070e32bc0970
oil <oilthepop@gmail.com>
2020-09-16 21:32:13 +0700

    first commit

---
+++ heros/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+
+from .models import Hero
+from .serializers import HeroSerializer
+
+class ListHero(generics.ListCreateAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer
+
+class DetailHero(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer
